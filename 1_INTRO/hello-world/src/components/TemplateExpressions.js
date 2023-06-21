275e98e28977cc6ee0b3bb033d00de5cba69fe65
Lucas <lucascomaru20@gmail.com>
2023-06-21 01:40:36 -0300

    Novas aulas

--- 1_INTRO/hello-world/src/components/TemplateExpressions.js
+++ 1_INTRO/hello-world/src/components/TemplateExpressions.js
@@ -1,12 +1,15 @@
-import React from 'react';
-
 const TemplateExpressions = () => {
 
     const name = 'Lucas'
+    const data = {
+        age:31,
+        job: 'Programador'
+    }
 
     return(
         <div>
             <h1>Olá {name}, seja bem vindo! </h1>
+            <p>Você atua como: {data.job}</p>
         </div>
     );
 };
