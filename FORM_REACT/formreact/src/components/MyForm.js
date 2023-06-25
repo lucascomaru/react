15dd8e7f9b1761d910130279517b8f5c71a04c4c
Lucas <lucascomaru20@gmail.com>
2023-06-25 17:17:13 -0300

    Nova aula

--- FORM_REACT/formreact/src/components/MyForm.js
+++ FORM_REACT/formreact/src/components/MyForm.js
@@ -1,12 +1,24 @@
 import './MyForm.css'
 
+import { useState} from 'react'
+
 const MyForm = () => {
+
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  };
+
+  console.log(name);
+
   return (
     <div>
         <form>
             <div>
                 <label htmlFor='name'>Nome: </label>
-                <input type='text' name='name' placeholder='Digite seu nome: '/>
+                <input type='text' name='name' placeholder='Digite seu nome: ' onChange={handleName}/>
             </div>
             <label>
                 <span>E-mail</span>
@@ -15,7 +27,7 @@ const MyForm = () => {
             <input type='submit' value='Enviar' />
         </form>
     </div>
-  )
-}
+  );
+};
 
-export default MyForm
+export default MyForm;
