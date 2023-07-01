be306610f0e43f9942b17ba7f54546f7a8b691ef
Lucas <lucascomaru20@gmail.com>
2023-07-01 17:43:33 -0300

    Novas aulas

---
+++ PROJETO_REACT/projetoadivinha/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MyComponent from './components/MyComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Meu App</h1>
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ PROJETO_REACT/projetoadivinha/src/components/MyComponent.jsx
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>Esse é o meu MyComponent</h1>
+    </div>
+  );
+};
+
+export default MyComponent;
